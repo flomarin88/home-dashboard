7e16c2d83d5e7e730b63003c377df730037ce16f
Florian Marin <fmarin@octo.com>
2015-10-25 21:53:33 +0100

    RATP : Traffic OK with API call

--- jobs/ratp.rb
+++ jobs/ratp.rb
@@ -3,9 +3,9 @@ require 'json'
 nextTrainsJSON = '{destination: "St-Germain-en-Laye Poissy-Cergy",ligne: "a",station: "la varenne-chennevieres",horaires: [{id: "WRWN",terminus: "La Varenne-Chennevieres",horaire: "Sans voyageurs V.Z"},{id: "WRWN",terminus: "La Varenne-Chennevieres",horaire: "Sans voyageurs V.Z"},{id: "WRWN",terminus: "La Varenne-Chennevieres",horaire: "Sans voyageurs V.Z"}]}'
 
 def getTrafficData
-	#trafficJSON = '{"trafic": "normal"}'
-	trafficJSON = '{"trafic": "perturbation","perturbations": [{"METRO 1": "Ts les jours l\'arrêt n\'est pas marqué à Louvre-Rivoli jusqu\'au 25/11/15. (travaux de rénovation)"},{"METRO 2": "Ts les jours le trafic est interrompu entre Nation et Avron jusqu\'au 27/10/15. (travaux sur la voie)"}]}'
-	return JSON.parse(trafficJSON)
+	trafficURI = URI('http://api-ratp.pierre-grimaud.fr/data/trafic/rer')
+	trafficDataResponse = Net::HTTP.get_response(trafficURI)
+	return JSON.parse(trafficDataResponse.body)
 end
 
 def getTrafficSituation(traffic)
@@ -25,10 +25,14 @@ def getLinesInformations(traffic)
 	return lines
 end
 
-SCHEDULER.every '5m', :first_in => 0 do |job|
+def trafficJob
 	traffic = getTrafficData()
 	situation = getTrafficSituation(traffic)
 	lines = getLinesInformations(traffic)
 	send_event('rer_traffic', { :situation => situation, :lines => lines })
+end
+
+SCHEDULER.every '5m', :first_in => 0 do |job|
+	trafficJob()
   	#send_event('rer_next_train', { :text => 'normal' })
 end
